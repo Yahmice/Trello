162af018749cb4be3942c3c830f6c3498b188e00
Yahmice <83534639+Yahmice@users.noreply.github.com>
2024-12-01 16:58:33 +0500

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
-import Column from '../components/column/Column';
-import Container from '../components/container/Container';
-import DragController from '../libs/DragController';
-import Storage from '../libs/Storage';
+import Column from '../components/column/column.js';
+import Container from '../components/container/container.js';
+import DragController from '../libs/dragController.js';
+import Storage from '../libs/storage.js';
 
 export default class App {
   constructor() {
